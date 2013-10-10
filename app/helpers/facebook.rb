f07ca3daad2e91dd1cbf1182e0b5215f2daf46a5
Maria Pacana <maria.pacana@gmail.com>
2013-10-10 13:58:59 -0400

    Removed www from redirect url

--- app/helpers/facebook.rb
+++ app/helpers/facebook.rb
@@ -1,7 +1,7 @@
 helpers do 
 
 	def display_oauth
-		callback_url =  "http://www.date-automator.herokuapp.com/oauth_fromfb"
+		callback_url =  "http://date-automator.herokuapp.com/oauth_fromfb"
 
 		@oauth = Koala::Facebook::OAuth.new(ENV['FB_ID'], ENV['FB_SECRET'], callback_url)
 		@oauth.url_for_oauth_code

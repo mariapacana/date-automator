0b9bf898e8148e3b4662dd3d07a446f10e758728
Maria Pacana <maria.pacana@gmail.com>
2013-10-16 15:57:19 -0400

    Debugging getting contact list with access token

--- app/helpers/google.rb
+++ app/helpers/google.rb
@@ -23,6 +23,8 @@ helpers do
   end
 
   def get_contacts
-  	redirect to("https://www.google.com/m8/feeds/contacts/#{currentuser.email}/full?token=#{session[:access_token]}")
+		token = session[:access_token]  	
+		p "https://www.google.com/m8/feeds/contacts/#{currentuser.email}/full?access_token=#{token}"
+  	# redirect to("https://www.google.com/m8/feeds/contacts/#{currentuser.email}/full?access_token=#{token}")
   end
 end

e4e72eeb718c5cdb6da30972dc84fe4483f9fb72
Maria Pacana <maria.pacana@gmail.com>
2013-10-16 13:16:59 -0400

    Temporarily getting rid of discovered_api

--- app/helpers/google.rb
+++ app/helpers/google.rb
@@ -8,7 +8,7 @@ helpers do
     client.authorization.client_secret = ENV['G_SECRET']
     client.authorization.redirect_uri = ENV['G_CALLBACK']
 		client.authorization.scope = ENV['G_CONTACTS_SCOPE']
-    contacts = client.discovered_api('contacts')
+    # contacts = client.discovered_api('contacts')
 
     p redirect_uri = client.authorization.authorization_uri
 	end

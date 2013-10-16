42360a8f942664f9370353a416a68004d3a36142
Maria Pacana <maria.pacana@gmail.com>
2013-10-16 14:36:12 -0400

    Wip debugging redirect to callback url part 11

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,9 +3,15 @@ get '/' do
 end
 
 get '/oauth_google' do
-  p "CALLBACK URL...........#{display_oauth_google}"
-
-  redirect to(display_oauth_google)
+  client = Google::APIClient.new((options = 
+                                 {application_name: "Date Automator", 
+                                  application_version: "1"}))
+  client.authorization.client_id = ENV['G_ID']
+  client.authorization.client_secret = ENV['G_SECRET']
+  client.authorization.redirect_uri = ENV['G_CALLBACK']
+  client.authorization.scope = ENV['G_CONTACTS_SCOPE']
+  p "AUTHORIZATION URI!!........................."
+  p client.authorization.authorization_uri
 end
 
 # get '/oauth2callback' do
